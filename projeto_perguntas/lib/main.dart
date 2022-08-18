b73926a404e96c55e2191658371441c41a59e6ec
Juliano <j.nishizima@hotmail.com>
2022-08-17 21:03:55 -0300

    passando um funcao callback

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -27,9 +27,9 @@ class _PerguntaAppState extends State<PerguntaApp>{
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntasSelecionada]),
-            Resposta('Resposta 1'),
-            Resposta('Resposta 2'),
-            Resposta('Resposta 3'),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         )
       ),
