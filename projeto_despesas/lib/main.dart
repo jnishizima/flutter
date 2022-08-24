f0688e871fc1d706badcb2174f0954a43c1bd393
Juliano <j.nishizima@hotmail.com>
2022-08-24 20:47:28 -0300

    criando o modelo transaction e o definido as colunas

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,20 +35,21 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Despesas Pessoais'),
       ),
       body: Column(
-        children: [
-          Container(
-            width: double.infinity,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          SizedBox(
             child: Card(
               color: Colors.blue,
-              child: Text('Grafico'),
+              child: Text('Gráfico'),
               elevation: 5,
             ),
           ),
           Card(
-            child: Text('Lista de Transações')
-          )
+            child: Text('Lista de Transações'),
+          ),
         ],
-      )
+      ),
     );
   }
 }
