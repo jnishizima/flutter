5550a476136f536b888450be6cb187d15aa926ba
Juliano <j.nishizima@hotmail.com>
2022-08-24 20:35:38 -0300

    combinando os dois widgets

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -20,9 +20,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
       ),
-      body:  const Center(
-        child: Text('Versão Inicial'),
-      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações')
+          )
+        ],
+      )
     );
   }
 }
