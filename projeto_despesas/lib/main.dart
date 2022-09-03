d9a4d5cfd26757c40e4c2f9b5ebeed21790671a1
Juliano <j.nishizima@hotmail.com>
2022-09-03 17:15:07 -0300

    adicionando o botao de transação

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -19,6 +19,9 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -35,6 +38,13 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
