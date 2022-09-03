e90c169a84474d3e7aaf8626b99f998dff6bf250
Juliano <j.nishizima@hotmail.com>
2022-09-03 16:29:38 -0300

    fazendo a transação do filho para o pai via funcao

--- projeto_despesas/lib/components/transaction_form.dart
+++ projeto_despesas/lib/components/transaction_form.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  TransactionForm({Key? key}) : super(key: key);
+  TransactionForm(this.onSubmit);
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +40,9 @@ class TransactionForm extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0 ;
+                    onSubmit(title, value);
                   },
                 )
               ],
